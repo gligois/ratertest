c8c8a2a3ab017e9dce8d2c254ad1cf62d3ab2203
Victor Gomez <victorgomez@skillup.es>
2020-04-16 15:59:53 +0200

    cambio presentacion y archivos

---
+++ parameters.js
@@ -0,0 +1,4 @@
+let keys = {
+    google: 'https://docs.google.com/spreadsheets/d/1Zc7PJoe_2rUbxaTWy0RCqwBlWhLn9vZNMUZlkhyz8Ns/edit?usp=sharing',
+    jsonstorage: "https://jsonstorage.net/api/items/758eb8f1-76ea-4d94-bbae-ce7673f7feea"
+}
